f057a096e103cd24c9bc75f7cd3568e72d5bffd1
goshan_tvi <h.qiu@tvisioninsights.com>
2017-03-31 19:10:40 +0900

    change command from config to init

--- lib/generators/redata/init_generator.rb
+++ lib/generators/redata/init_generator.rb
@@ -1,11 +1,11 @@
 require 'rails/generators/base'
 
 module Redata
-  class ConfigGenerator < Rails::Generators::Base
+  class InitGenerator < Rails::Generators::Base
     source_root File.expand_path('../../templates', __FILE__)
     desc 'Redata Config'
 
-    def config_steps
+    def init_steps
       copy_file 'source_query_example.red.sql', 'query/sources/sources_query_example.red.sql'
       copy_file 'adjust_query_example.red.sql', 'query/adjust/adjust_query_example.red.sql'
       copy_file 'redata.yml', 'config/redata.yml'
